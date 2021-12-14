bc5b2726e246edde436ecee6c4ca0adca8f49048
Takeyuki UEDA <ueda@latelierdueda.com>
2021-12-14 18:42:52 +0900

    add router, vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.prototype.$apiKey = process.env.VUE_APP_apiKey
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+  <input v-model="playlistId" placeholder="playlistId">
+
+  {
+  "memo1": "",
+  "title": "",
+  "items": [
+  <div v-for="(item, i) in items" :key="i">
+    {"text": "{{item.snippet.title}}", 
+    "content": "{{item.snippet.resourceId.videoId}}"}
+    <!-- https://stackoverflow.com/questions/42740105/vue-check-if-you-are-on-the-last-prop-of-a-v-for-loop -->
+    <span v-if="i < items.length -1">,</span>
+  </div>
+    ]
+  }
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    playlistId: "",
+    items: ""
+  }),
+  methods: {
+    getPlayListItems: async function(playlistId){
+      console.log("key",this.$apiKey)
+      const result = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems`, {
+      params: {
+        part: 'id,snippet',
+        maxResults: 50,
+        playlistId: playlistId,
+        key: this.$apiKey
+      }
+    });
+    console.log(result.data)
+    this.items = result.data.items
+    }
+  },
+  mounted: function(){
+    this.getPlayListItems()
+  },
+  watch: {
+    playlistId: function(newPlaylistId){
+      this.getPlayListItems(newPlaylistId)
+    }
+  }
+}
+</script>
