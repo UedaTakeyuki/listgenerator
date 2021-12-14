997067533135708a5be44d0d0508935217fb25f9
Takeyuki UEDA <ueda@latelierdueda.com>
2021-12-14 18:06:43 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <input v-model="playlistId" placeholder="playlistId">
+
+{
+  "memo1": "",
+  "title": "",
+  "items": [
+  <div v-for="(item, i) in items" :key="i">
+    {"text": "{{item.snippet.title}}", 
+    "content": "{{item.snippet.resourceId.videoId}}"}
+    <!-- https://stackoverflow.com/questions/42740105/vue-check-if-you-are-on-the-last-prop-of-a-v-for-loop -->
+    <span v-if="i < items.length -1">,</span>
+  </div>
+    ]
+  }
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    playlistId: "",
+    items: ""
+  }),
+  methods: {
+    getPlayListItems: async function(playlistId){
+      console.log("key",this.$apiKey)
+      const result = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems`, {
+      params: {
+        part: 'id,snippet',
+        maxResults: 50,
+        playlistId: playlistId,
+        key: this.$apiKey
+      }
+    });
+    console.log(result.data)
+    this.items = result.data.items
+    }
+  },
+  mounted: function(){
+    this.getPlayListItems()
+  },
+  watch: {
+    playlistId: function(newPlaylistId){
+      this.getPlayListItems(newPlaylistId)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.prototype.$apiKey = process.env.VUE_APP_apiKey
 
 new Vue({
   render: h => h(App),
